2f888b12e938ac6f12aee05d6e73b10186b82272
Ana Vukoja <avukoj01@fesb.hr>
2025-03-17 20:36:43 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,3 +3,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Novo");
 Console.WriteLine("Nadodano");
 Console.WriteLine("Nadodano opet");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Novo");
+Console.WriteLine("Nadodano");
+Console.WriteLine("Nadodano opet");
